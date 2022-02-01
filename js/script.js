44c8f71538929c14c11af8f0357dd34c9102533f
Maeve <88163638+MaeveS2@users.noreply.github.com>
2022-02-01 20:58:38 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const elem = document.getElementById("amognus");
+let size = 10;
+let goUp = true;
+setInterval(() => {
+  if(goUp) {
+    size += 5;
+  } else {
+  	size -= 5;
+  }
+	elem.style.fontSize = size + 'px';
+  if(size >= 100) {
+   goUp = false
+  }
+}, 500)
