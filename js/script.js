b7ae598b93157ae2e93a1cf5ce2868e20731c1e7
Maeve <88163638+MaeveS2@users.noreply.github.com>
2022-02-01 20:59:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ const elem = document.getElementById("amognus");
 let size = 10;
 let goUp = true;
 setInterval(() => {
-  if(goUp) {
+  if (goUp) {
     size += 5;
   } else {
-  	size -= 5;
+    size -= 5;
   }
-	elem.style.fontSize = size + 'px';
-  if(size >= 100) {
-   goUp = false
+  elem.style.fontSize = size + 'px';
+  if (size >= 100) {
+    goUp = false
+  }
+
+  if (size <= 10) {
+    goUp = true;
   }
 }, 500)
