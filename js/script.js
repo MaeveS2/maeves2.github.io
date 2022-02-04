eda25df1368e57be074d42a34c9b73a1ff932e05
Maeve <88163638+MaeveS2@users.noreply.github.com>
2022-02-04 23:39:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,57 @@
 const elem = document.getElementById("amognus");
 let size = 10;
 let goUp = true;
+let movingRight = true;
+let movingDown = true;
+let x = 0;
+let y = 0
+let deg = 0;
+let moveAmount = 10;
+console.log(elem)
+
 setInterval(() => {
-  if (goUp) {
+  if(goUp) {
     size += 5;
   } else {
-    size -= 5;
+  	size -= 5;
   }
-  elem.style.fontSize = size + 'px';
-  if (size >= 100) {
-    goUp = false
+	elem.style.fontSize = size + 'px';
+  if(size >= 100) {
+   goUp = false
   }
-
-  if (size <= 10) {
-    goUp = true;
+  
+  if(size <= 10) {
+   goUp = true;
   }
 }, 500)
+
+
+
+const updateHandler = () => {
+    if (movingRight) {
+        elem.style.left = (x += moveAmount) + 'px';
+    } else {
+        elem.style.left = (x -= moveAmount) + 'px';
+    }
+
+    if (movingDown) {
+        elem.style.top = (y += moveAmount) + 'px';
+    } else {
+        elem.style.top = (y -= moveAmount) + 'px';
+    }
+
+    if (x >= window.innerWidth - elem.offsetWidth || x <= 0) {
+        movingRight = !movingRight;
+    }
+
+    if (y >= window.innerHeight - elem.offsetHeight || y <= 0) {
+        movingDown = !movingDown;
+    }
+
+    elem.style.transform = `rotate(${(deg += 5)}deg)`;
+    if (deg === 360) {
+        deg = 0;
+    }
+}
+
+setInterval(updateHandler, 500);
